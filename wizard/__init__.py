99f0fd7afbc77235cea8f121d49bde4083798245
Manuel Gonzalez <herohat@gmail.com>
2019-04-16 18:59:17 -0400

    cleaning

--- wizard/__init__.py
+++ wizard/__init__.py
@@ -20,7 +20,6 @@
 
 # You should have received a copy of the GNU General Public License
 # along with NCF DGII Reports.  If not, see <http://www.gnu.org/licenses/>.
-# ######################################################################
-from . import models
-from . import report
+# ######################################################################.
+
 
